a23648d6ab860abaedf9e01b3ffcc80c7fd5b1de
Наталья Столярова <semechkina.n@yandex.ru>
2023-06-18 12:23:11 +0300

    add secrets to env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,18 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret-key')
 
-DEBUG = True
+DEBUG = bool(os.getenv('DEB', ''))
 
-ALLOWED_HOSTS = ['kittygram-yandex.hopto.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('CLOUD_IP'), os.getenv(
+    'LOCAL'), os.getenv('LOCALHOST'), os.getenv('DOMEN_NAME')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
