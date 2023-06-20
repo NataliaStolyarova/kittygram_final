e19373b6b84b776762a6eb12aae085c7d7beeb76
Наталья Столярова <semechkina.n@yandex.ru>
2023-06-20 23:01:18 +0300

    add entrypoint to dockerfile

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate
 sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
 sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /static/static/
